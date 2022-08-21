5de846cde402d3ac88453c941e9a301efbb6759b
thyiuaa <thyiuaa@protonmail.com>
2022-08-21 20:49:33 +0800

    Fix folder structure

--- part_4/controllers/blogs.js
+++ part_4/controllers/blogs.js
@@ -1,35 +1,34 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({})
+
+  response.json(blogs)
 })
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body)
+
   if (blog.title === undefined || blog.url === undefined) {
     response.status(400).end()
     return
   }
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
+  const result = blog.save()
+
+  response.status(201).json(result)
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
   await Blog.findByIdAndRemove(request.params.id)
+
   response.status(204).end()
 })
 
 blogsRouter.put('/:id', async (request, response) => {
   await Blog.findByIdAndUpdate(request.params.id, request.body.update)
+
   response.status(204).end()
 })
 
